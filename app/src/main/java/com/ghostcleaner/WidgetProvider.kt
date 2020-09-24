0efb7ffee605eab7e8e46d1008400bf4f8c4b1bb
vlad <vladkalyuzhnyu@gmail.com>
2020-09-24 17:24:06 +0300

    changes

---
+++ app/src/main/java/com/ghostcleaner/WidgetProvider.kt
@@ -0,0 +1,106 @@
+package com.ghostcleaner
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
+import android.net.Uri
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.annotation.UiThread
+import com.ghostcleaner.extension.appWidgetManager
+import com.ghostcleaner.extension.pendingReceiverFor
+import org.jetbrains.anko.intentFor
+import timber.log.Timber
+
+class WidgetProvider : AppWidgetProvider() {
+
+    @UiThread
+    override fun onUpdate(context: Context, manager: AppWidgetManager, ids: IntArray) {
+        with(context) {
+            for (id in ids) {
+                updateWidget(id)
+            }
+        }
+    }
+
+    @UiThread
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        manager: AppWidgetManager,
+        id: Int,
+        newOptions: Bundle?
+    ) {
+        onUpdate(context, manager, intArrayOf(id))
+    }
+
+    @UiThread
+    override fun onReceive(context: Context, intent: Intent) {
+        with(context) {
+            when (val action = intent.action) {
+                else -> Timber.d("onReceive action=$action")
+            }
+            super.onReceive(context, intent)
+        }
+    }
+
+    companion object {
+
+        private val Context.widget: ComponentName
+            get() = ComponentName(applicationContext, WidgetProvider::class.java)
+
+        fun toggle(context: Context, enable: Boolean) {
+            context.packageManager.setComponentEnabledSetting(
+                context.widget,
+                if (enable) PackageManager.COMPONENT_ENABLED_STATE_ENABLED else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        }
+
+        /**
+         * @param force if true also make http requests besides ui updates
+         */
+        fun updateAll(context: Context, force: Boolean) {
+            with(context) {
+                val ids = appWidgetManager.getAppWidgetIds(widget)
+                if (ids.isNotEmpty()) {
+                    sendBroadcast(intentFor<WidgetProvider>().apply {
+                        action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+                        putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+                        putExtra("force", force)
+                    })
+                }
+            }
+        }
+
+        private fun Context.updateWidget(id: Int) {
+            val options = appWidgetManager.getAppWidgetOptions(id)
+            val isPortrait = resources.configuration.orientation == ORIENTATION_PORTRAIT
+            val width = options.getInt(
+                if (isPortrait) {
+                    AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH
+                } else {
+                    AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH
+                }
+            )
+            Timber.d("updateWidget isPortrait=$isPortrait")
+            appWidgetManager.updateAppWidget(
+                id, RemoteViews(packageName, R.layout.widget_layout).apply {
+
+                }
+            )
+        }
+
+        private fun Context.getClickIntent(widgetId: Int, action: String): PendingIntent {
+            return pendingReceiverFor(intentFor<WidgetProvider>().also {
+                it.action = action
+                it.data = Uri.parse(it.toUri(Intent.URI_INTENT_SCHEME))
+                it.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+            }, widgetId)
+        }
+    }
+}
---
+++ app/src/main/java/com/ghostcleaner/extension/RemoteViews.kt
@@ -0,0 +1,17 @@
+package com.ghostcleaner.extension
+
+import android.widget.RemoteViews
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+
+fun RemoteViews.setEnabled(viewId: Int, enabled: Boolean) {
+    setBoolean(viewId, "setEnabled", enabled)
+}
+
+fun RemoteViews.setBackgroundColor(viewId: Int, @ColorInt value: Int) {
+    setInt(viewId, "setBackgroundColor", value)
+}
+
+fun RemoteViews.setBackgroundRes(viewId: Int, @DrawableRes value: Int) {
+    setInt(viewId, "setBackgroundResource", value)
+}
