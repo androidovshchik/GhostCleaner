fe1064603e0d7839577989dbd7db98109602bfc7
vlad <vladkalyuzhnyu@gmail.com>
2020-09-26 09:12:36 +0300

    changes

--- app/src/main/java/com/ghostcleaner/GlobalVars.kt
+++ app/src/main/java/com/ghostcleaner/GlobalVars.kt
@@ -2,11 +2,9 @@ package com.ghostcleaner
 
 import org.threeten.bp.format.DateTimeFormatter
 
-const val ACTION_NONE = "action_none"
-const val ACTION_DONE = "action_done"
-const val ACTION_DELETE = "action_delete"
-const val ACTION_SKIP = "action_skip"
-const val ACTION_REFRESH = "action_refresh"
-const val ACTION_OPEN = "action_open"
+const val ACTION_ROCKET = "action_rocket"
+const val ACTION_BATTERY = "action_battery"
+const val ACTION_TEMP = "action_temperature"
+const val ACTION_TRASH = "action_trash"
 
 val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
