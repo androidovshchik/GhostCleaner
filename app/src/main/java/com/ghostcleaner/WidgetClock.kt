b249d5f9c0ad3e835c764b6bf494eb19a4e70e81
vlad <vladkalyuzhnyu@gmail.com>
2020-09-26 10:46:09 +0300

    changes

--- app/src/main/java/com/ghostcleaner/WidgetClock.kt
+++ app/src/main/java/com/ghostcleaner/WidgetClock.kt
@@ -1,21 +1,32 @@
 package com.ghostcleaner
 
+import android.app.AlarmManager
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.os.BatteryManager
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.RemoteViews
 import androidx.annotation.UiThread
+import androidx.core.app.AlarmManagerCompat
 import com.ghostcleaner.extension.appWidgetManager
+import com.ghostcleaner.extension.getComponent
+import com.ghostcleaner.extension.pendingReceiverFor
+import org.jetbrains.anko.alarmManager
 import org.jetbrains.anko.batteryManager
+import org.threeten.bp.Duration
 import org.threeten.bp.LocalTime
+import org.threeten.bp.temporal.ChronoUnit
 import timber.log.Timber
 
 class WidgetClock : AppWidgetProvider() {
+
+    override fun onEnabled(context: Context) {
+        setAlarm(context)
+    }
 
     @UiThread
     override fun onUpdate(context: Context, manager: AppWidgetManager, ids: IntArray) {
@@ -41,12 +52,12 @@ class WidgetClock : AppWidgetProvider() {
     @UiThread
     override fun onReceive(context: Context, intent: Intent) {
         with(context) {
+            val ids = appWidgetManager.getAppWidgetIds(getComponent<WidgetClock>())
             when (val action = intent.action) {
                 Intent.ACTION_TIME_TICK, Intent.ACTION_TIME_CHANGED, Intent.ACTION_TIMEZONE_CHANGED,
                 Intent.ACTION_LOCALE_CHANGED, Intent.ACTION_BATTERY_CHANGED, Intent.ACTION_BATTERY_LOW,
                 Intent.ACTION_BATTERY_OKAY, Intent.ACTION_SCREEN_ON -> {
                     Timber.d("onReceive action=$action")
-                    val ids = appWidgetManager.getAppWidgetIds(widget)
                     if (ids.isNotEmpty()) {
                         onUpdate(applicationContext, appWidgetManager, ids)
                     }
@@ -54,13 +65,28 @@ class WidgetClock : AppWidgetProvider() {
                 else -> Timber.d("onReceive action=$action")
             }
             super.onReceive(context, intent)
+            if (ids.isNotEmpty()) {
+                val start = LocalTime.now()
+                val end = start.plusMinutes(1).truncatedTo(ChronoUnit.MINUTES)
+                val millis = Duration.between(start, end).toMillis()
+                Timber.d("Alarm millis=$millis")
+                AlarmManagerCompat.setExact(
+                    alarmManager,
+                    AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime() + millis,
+                    pendingReceiverFor<WidgetClock>()
+                )
+            }
+            setAlarm(applicationContext)
+        }
+    }
+
+    private fun setAlarm(context: Context) {
+        with(context) {
         }
     }
 
     companion object {
-
-        private val Context.widget: ComponentName
-            get() = ComponentName(applicationContext, WidgetClock::class.java)
 
         private fun Context.updateWidget(id: Int, time: String, level: Int) {
             val options = appWidgetManager.getAppWidgetOptions(id)
@@ -76,7 +102,8 @@ class WidgetClock : AppWidgetProvider() {
             appWidgetManager.updateAppWidget(
                 id, RemoteViews(packageName, R.layout.widget_clock).apply {
                     setTextViewText(R.id.tv_clock, time)
-                    setTextViewText(R.id.tv_time, "")
+                    // todo
+                    setTextViewText(R.id.tv_time, "8h 5m remaining")
                     setTextViewText(R.id.tv_battery, "$level%")
                 }
             )
