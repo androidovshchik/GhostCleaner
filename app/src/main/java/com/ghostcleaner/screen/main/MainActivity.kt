cf5f3ae73da9c78d26f029f9ceb79a8f7f314365
vlad <vladkalyuzhnyu@gmail.com>
2020-09-26 13:48:22 +0300

    changes

--- app/src/main/java/com/ghostcleaner/screen/main/MainActivity.kt
+++ app/src/main/java/com/ghostcleaner/screen/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ghostcleaner.screen.main
 
 import android.os.Bundle
+import com.ghostcleaner.ACTION_BATTERY
+import com.ghostcleaner.ACTION_TEMPERATURE
+import com.ghostcleaner.ACTION_TRASH
 import com.ghostcleaner.R
 import com.ghostcleaner.screen.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,8 +14,14 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val adapter = TabsAdapter(supportFragmentManager)
-        vp_main.adapter = TabsAdapter(supportFragmentManager)
+        vp_main.adapter = adapter
         vp_main.offscreenPageLimit = adapter.count
+        when (intent.getStringExtra("action")) {
+            ACTION_BATTERY -> vp_main.setCurrentItem(1, false)
+            ACTION_TEMPERATURE -> vp_main.setCurrentItem(2, false)
+            ACTION_TRASH -> vp_main.setCurrentItem(3, false)
+            else -> vp_main.setCurrentItem(0, false)
+        }
         bn_main.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.action_rocket -> vp_main.setCurrentItem(0, true)
