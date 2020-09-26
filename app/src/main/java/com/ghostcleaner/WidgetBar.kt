d24b16da78c3a3413a603d36523e845b15a17ab0
vlad <vladkalyuzhnyu@gmail.com>
2020-09-26 09:08:46 +0300

    changes

--- app/src/main/java/com/ghostcleaner/WidgetBar.kt
+++ app/src/main/java/com/ghostcleaner/WidgetBar.kt
@@ -2,7 +2,6 @@ package com.ghostcleaner
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
@@ -11,6 +10,9 @@ import android.widget.RemoteViews
 import androidx.annotation.UiThread
 import com.ghostcleaner.extension.appWidgetManager
 import com.ghostcleaner.extension.getClickIntent
+import com.ghostcleaner.screen.SplashActivity
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
 import timber.log.Timber
 
 class WidgetBar : AppWidgetProvider() {
@@ -38,6 +40,10 @@ class WidgetBar : AppWidgetProvider() {
     override fun onReceive(context: Context, intent: Intent) {
         with(context) {
             when (val action = intent.action) {
+                ACTION_ROCKET, ACTION_BATTERY, ACTION_TEMP, ACTION_TRASH -> {
+                    Timber.d("onReceive action=$action")
+                    startActivity(intentFor<SplashActivity>("action" to action).newTask())
+                }
                 else -> Timber.d("onReceive action=$action")
             }
             super.onReceive(context, intent)
@@ -46,9 +52,14 @@ class WidgetBar : AppWidgetProvider() {
 
     companion object {
 
-        private val Context.widget: ComponentName
-            get() = ComponentName(applicationContext, WidgetBar::class.java)
-
+        /**
+         * 1 | 1 1 0 0 | 1
+         * 2 | 1 0 1 0 | 2
+         * 3 | 1 0 0 1 | 3
+         * 4 | 0 1 1 0 | 4
+         * 5 | 0 1 0 1 | 5
+         * 6 | 0 0 1 1 | 6
+         */
         private fun Context.updateWidget(id: Int) {
             val options = appWidgetManager.getAppWidgetOptions(id)
             val isPortrait = resources.configuration.orientation == ORIENTATION_PORTRAIT
@@ -62,10 +73,27 @@ class WidgetBar : AppWidgetProvider() {
             Timber.d("updateWidget isPortrait=$isPortrait width=$width")
             appWidgetManager.updateAppWidget(
                 id, RemoteViews(packageName, R.layout.widget_bar).apply {
-                    setOnClickPendingIntent(R.id.ib_rocket, getClickIntent(id, ACTION_DONE))
-                    setOnClickPendingIntent(R.id.ib_battery, getClickIntent(id, ACTION_DONE))
-                    setOnClickPendingIntent(R.id.ib_temperature, getClickIntent(id, ACTION_DONE))
-                    setOnClickPendingIntent(R.id.ib_trash, getClickIntent(id, ACTION_DONE))
+                    val n = (1..6).random()
+                    setImageViewResource(
+                        R.id.ib_rocket,
+                        if (n in 1..3) R.drawable.ic_rocket_red else R.drawable.ic_rocket
+                    )
+                    setImageViewResource(
+                        R.id.ib_battery,
+                        if (n == 1 || n in 4..5) R.drawable.ic_battery_red else R.drawable.ic_battery
+                    )
+                    setImageViewResource(
+                        R.id.ib_temperature,
+                        if (n % 2 == 0) R.drawable.ic_temperature_red else R.drawable.ic_temperature
+                    )
+                    setImageViewResource(
+                        R.id.ib_trash,
+                        if (n == 3 || n > 4) R.drawable.ic_trash_red else R.drawable.ic_trash
+                    )
+                    setOnClickPendingIntent(R.id.ib_rocket, getClickIntent(id, ACTION_ROCKET))
+                    setOnClickPendingIntent(R.id.ib_battery, getClickIntent(id, ACTION_BATTERY))
+                    setOnClickPendingIntent(R.id.ib_temperature, getClickIntent(id, ACTION_TEMP))
+                    setOnClickPendingIntent(R.id.ib_trash, getClickIntent(id, ACTION_TRASH))
                 }
             )
         }
