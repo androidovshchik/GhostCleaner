911a09610fb7ac0a02813bb565e38c9127500ce6
vlad <vladkalyuzhnyu@gmail.com>
2020-10-08 12:01:54 +0300

    changes

--- app/src/main/java/com/ghostcleaner/screen/DoneActivity.kt
+++ app/src/main/java/com/ghostcleaner/screen/DoneActivity.kt
@@ -3,11 +3,18 @@ package com.ghostcleaner.screen
 import android.os.Bundle
 import com.ghostcleaner.R
 import com.ghostcleaner.screen.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_done.*
 
 class DoneActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_done)
+        btn_home.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
     }
 }
--- app/src/main/java/com/ghostcleaner/service/Quadruple.kt
+++ app/src/main/java/com/ghostcleaner/service/Quadruple.kt
@@ -1,8 +1,8 @@
 package com.ghostcleaner.service
 
 data class Quadruple<out A, out B, out C, out D>(
-    public val first: A,
-    public val second: B,
-    public val third: C,
-    public val fourth: D
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D
 )
