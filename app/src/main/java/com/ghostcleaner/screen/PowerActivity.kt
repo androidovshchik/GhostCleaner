cfd4c51ad0f6d053a6b7656029e6f85a4e442933
vlad <vladkalyuzhnyu@gmail.com>
2020-10-10 08:45:14 +0300

    changes

--- app/src/main/java/com/ghostcleaner/screen/PowerActivity.kt
+++ app/src/main/java/com/ghostcleaner/screen/PowerActivity.kt
@@ -8,7 +8,6 @@ import com.ghostcleaner.screen.base.BaseActivity
 import com.ghostcleaner.service.BatteryMode
 import com.ghostcleaner.service.EnergyManager
 import kotlinx.android.synthetic.main.activity_power.*
-import kotlinx.android.synthetic.main.activity_scanning.*
 
 class PowerActivity : BaseActivity() {
 
@@ -20,30 +19,30 @@ class PowerActivity : BaseActivity() {
         val mode = BatteryMode.valueOf(intent.getStringExtra("mode")!!)
         val manager = EnergyManager.getInstance(applicationContext)
         btn_apply.setOnClickListener {
-            tv_line1.setTextColor(grayColor)
-            tv_line2.setTextColor(grayColor)
-            tv_line3.setTextColor(grayColor)
-            tv_line4.setTextColor(grayColor)
+            tv_text1.setTextColor(grayColor)
+            tv_text2.setTextColor(grayColor)
+            tv_text3.setTextColor(grayColor)
+            tv_text4.setTextColor(grayColor)
             manager.optimize(mode)
         }
         val list = manager.getDescList(mode)
-        tv_line1.text = list.getOrNull(0)
-        tv_line2.text = list.getOrNull(1)
-        tv_line3.text = list.getOrNull(2)
-        tv_line4.text = list.getOrNull(3)
+        tv_text1.text = list.getOrNull(0)
+        tv_text2.text = list.getOrNull(1)
+        tv_text3.text = list.getOrNull(2)
+        tv_text4.text = list.getOrNull(3)
         manager.optimization.observeFreshly(this, {
             when (it) {
                 1 -> {
-                    tv_line1.setTextColor(Color.WHITE)
+                    tv_text1.setTextColor(Color.WHITE)
                 }
                 2 -> {
-                    tv_line2.setTextColor(Color.WHITE)
+                    tv_text2.setTextColor(Color.WHITE)
                 }
                 3 -> {
-                    tv_line3.setTextColor(Color.WHITE)
+                    tv_text3.setTextColor(Color.WHITE)
                 }
                 else -> {
-                    tv_line4.setTextColor(Color.WHITE)
+                    tv_text4.setTextColor(Color.WHITE)
                 }
             }
         })
