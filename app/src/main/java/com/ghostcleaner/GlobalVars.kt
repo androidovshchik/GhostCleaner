c99b332fc49c0ef712ec7406b7ab39c5296d6bb6
vlad <vladkalyuzhnyu@gmail.com>
2020-10-11 08:34:32 +0300

    changes

--- app/src/main/java/com/ghostcleaner/GlobalVars.kt
+++ app/src/main/java/com/ghostcleaner/GlobalVars.kt
@@ -4,6 +4,7 @@ import org.threeten.bp.format.DateTimeFormatter
 
 const val REQUEST_SETTINGS = 100
 const val REQUEST_STORAGE = 200
+const val REQUEST_ADS = 300
 
 const val ACTION_ROCKET = "action_rocket"
 const val ACTION_BATTERY = "action_battery"
