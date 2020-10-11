9d440c1e718112fd5f4b633143215b354e9f795c
vlad <vladkalyuzhnyu@gmail.com>
2020-10-11 21:47:48 +0300

    changes

--- app/src/main/java/com/ghostcleaner/view/NoAdsLayout.kt
+++ app/src/main/java/com/ghostcleaner/view/NoAdsLayout.kt
@@ -8,12 +8,17 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
+import com.ghostcleaner.Preferences
 import com.ghostcleaner.R
+import com.ghostcleaner.service.AdmobClient
 import kotlinx.android.synthetic.main.merge_no_ads.view.*
 import kotlinx.coroutines.*
 import timber.log.Timber
 
-class NoAdsLayout : RelativeLayout, CoroutineScope {
+class NoAdsLayout : RelativeLayout, CoroutineScope, Observer<Boolean> {
 
     private val job = SupervisorJob()
 
@@ -40,10 +45,18 @@ class NoAdsLayout : RelativeLayout, CoroutineScope {
     @SuppressLint("Recycle")
     private fun init(attrs: AttributeSet?) {
         View.inflate(context, R.layout.merge_no_ads, this)
+        val preferences = Preferences(context)
+        if (!preferences.enableAds) {
+            isInvisible = true
+        }
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        AdmobClient.getInstance(context).enableAds.observeForever(this)
+        if (!isVisible) {
+            return
+        }
         val green = ContextCompat.getColor(context, R.color.colorGreen)
         val maxR = 20f
         val frames = 60
@@ -64,8 +77,16 @@ class NoAdsLayout : RelativeLayout, CoroutineScope {
         }
     }
 
+    override fun onChanged(t: Boolean) {
+        if (!t) {
+            job.cancelChildren()
+            isInvisible = true
+        }
+    }
+
     override fun onDetachedFromWindow() {
         job.cancelChildren()
+        AdmobClient.getInstance(context).enableAds.removeObserver(this)
         super.onDetachedFromWindow()
     }
 
