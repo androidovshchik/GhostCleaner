31082bb6538cccbc40918d830c83a56487c5f5b7
vlad <vladkalyuzhnyu@gmail.com>
2020-10-23 14:25:04 +0300

    d views

---
+++ app/src/main/java/com/ghostcleaner/view/DMaterialButton.kt
@@ -0,0 +1,27 @@
+package com.ghostcleaner.view
+
+import android.content.Context
+import android.util.AttributeSet
+import com.ghostcleaner.R
+import com.ghostcleaner.extension.use
+import com.ghostcleaner.service.D
+import com.google.android.material.button.MaterialButton
+
+class DMaterialButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : MaterialButton(context, attrs, defStyleAttr) {
+
+    init {
+        if (attrs != null) {
+            context.obtainStyledAttributes(attrs, R.styleable.DMaterialButton).use {
+                getString(R.styleable.DMaterialButton_d)?.let {
+                    text = D[it]
+                }
+            }
+        }
+    }
+
+    override fun hasOverlappingRendering() = false
+}
---
+++ app/src/main/java/com/ghostcleaner/view/DTextView.kt
@@ -0,0 +1,27 @@
+package com.ghostcleaner.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.ghostcleaner.R
+import com.ghostcleaner.extension.use
+import com.ghostcleaner.service.D
+
+class DTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    init {
+        if (attrs != null) {
+            context.obtainStyledAttributes(attrs, R.styleable.DTextView).use {
+                getString(R.styleable.DTextView_d)?.let {
+                    text = D[it]
+                }
+            }
+        }
+    }
+
+    override fun hasOverlappingRendering() = false
+}
