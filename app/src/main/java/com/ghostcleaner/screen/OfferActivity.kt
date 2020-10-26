c6543468356e2a8dc7395262240228e1cca6043c
vlad <vladkalyuzhnyu@gmail.com>
2020-10-26 15:57:36 +0300

    timer

--- app/src/main/java/com/ghostcleaner/screen/OfferActivity.kt
+++ app/src/main/java/com/ghostcleaner/screen/OfferActivity.kt
@@ -1,10 +1,12 @@
 package com.ghostcleaner.screen
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.ghostcleaner.R
 import com.ghostcleaner.screen.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_offer.*
 import org.jetbrains.anko.intentFor
+import java.util.concurrent.TimeUnit
 
 class OfferActivity : BaseActivity() {
 
@@ -18,5 +20,19 @@ class OfferActivity : BaseActivity() {
             startActivity(intentFor<BuyActivity>().putExtras(intent))
             finish()
         }
+        val min = TimeUnit.MINUTES.toMillis(5)
+        val max = TimeUnit.MINUTES.toMillis(10)
+        val total = (min..max).random()
+        object : CountDownTimer(total, 1000) {
+
+            override fun onTick(millis: Long) {
+                val seconds = millis / 1000
+                tv_time.text = String.format("00:%02d:%02d", seconds / 60, seconds % 60)
+            }
+
+            override fun onFinish() {
+                finish()
+            }
+        }.start()
     }
 }
