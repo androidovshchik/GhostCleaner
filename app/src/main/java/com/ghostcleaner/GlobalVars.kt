9e9e74b6bc7cb8a69feaee32f35fc5660ea849bb
vlad <vladkalyuzhnyu@gmail.com>
2021-01-19 22:36:48 +0300

    changes

--- app/src/main/java/com/ghostcleaner/GlobalVars.kt
+++ app/src/main/java/com/ghostcleaner/GlobalVars.kt
@@ -17,6 +17,6 @@ const val EXTRA_ACTION = "action"
 const val EXTRA_TITLE = "title_key"
 
 const val SKU_ADS = "disabledads"
-const val SUB_ADS = "sub_ads"
+const val SUB_ADS = "sub_ads2"
 
 val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
